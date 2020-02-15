3171fe9f5eea6323790c8eb6e4722061a856bed4
Afuza <thegannoz@gmail.com>
2020-02-15 09:38:59 +0700

    Created SideDrawer, Backdrop component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./containers/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
 
 const app = () => {
   return (
     <div>
       <Navbar />
+      <SideDrawer />
       <Switch>
         <Route path="/" component={Home} />
       </Switch>
