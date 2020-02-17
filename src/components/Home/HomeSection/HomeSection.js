b35d2bf05a460a96145bc86fdee60f9e00396e5d
Afuza <thegannoz@gmail.com>
2020-02-17 13:53:03 +0700

    Created HomeSection
    HomeSection will be the first section in page

---
+++ src/components/Home/HomeSection/HomeSection.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+import classes from "./HomeSection.module.css";
+
+class HomeSection extends Component {
+  state = {};
+  render() {
+    return <section id="home" className={classes.HomeSection}></section>;
+  }
+}
+
+export default HomeSection;
