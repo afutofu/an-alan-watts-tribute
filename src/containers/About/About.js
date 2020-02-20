0d3d75eb67df1f584729aa8790fe568411c45b51
Afuza <thegannoz@gmail.com>
2020-02-20 08:06:50 +0700

    Added MiddleYearsSection, configured links
    MiddleYearsSection added to about route, links properly configured

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -4,6 +4,7 @@ import classes from "./About.module.css";
 import HomeSection from "../../components/HomeSection/HomeSection";
 import AboutSection from "../../components/About/AboutSection/AboutSection";
 import EarlyLifeSection from "../../components/About/EarlyLifeSection/EarlyLifeSection";
+import MiddleYearsSection from "../../components/About/MiddleYearsSection/MiddleYearsSection";
 
 class About extends Component {
   state = {};
@@ -13,6 +14,7 @@ class About extends Component {
         <HomeSection order="0" />
         <AboutSection order="1" />
         <EarlyLifeSection order="2" />
+        <MiddleYearsSection order="3" />
       </main>
     );
   }
