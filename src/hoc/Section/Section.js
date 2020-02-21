0d58abdb785198b134bc7d6d61cf0f84b61cad33
Afuza <thegannoz@gmail.com>
2020-02-21 10:13:45 +0700

    Improved Section hoc

--- src/hoc/Section/Section.js
+++ src/hoc/Section/Section.js
@@ -5,12 +5,38 @@ import TopSectionChanger from "../../components/UI/TopSectionChanger/TopSectionC
 import SectionChanger from "../../components/UI/SectionChanger/SectionChanger";
 
 const section = props => {
+  const link = () => {
+    if (props.link) {
+      return (
+        <a href={props.link} style={{ color: props.color }}>
+          {props.linkContent}
+        </a>
+      );
+    }
+    return null;
+  };
+
   return (
-    <section>
-      <TopSectionChanger color={props.color} />
-      {props.children}
-      <a href={props.link}>{props.linkContent}</a>
-      <SectionChanger to={props.nextSection} color={props.color} />
+    <section
+      className={classes.Section}
+      style={{ backgroundImage: props.background }}
+      id={props.id}
+    >
+      <TopSectionChanger color={props.color} topPosition={props.order} />
+      <div className={classes.Content}>
+        <h3 className={classes.Title} style={{ color: props.color }}>
+          {props.title}
+        </h3>
+        <div className={classes.Container} style={{ color: props.color }}>
+          {props.mainContent}
+        </div>
+        {link()}
+      </div>
+      <SectionChanger
+        to={props.nextSection}
+        color={props.color}
+        topPosition={props.order}
+      />
     </section>
   );
 };
