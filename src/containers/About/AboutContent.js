db3c7d4e9e7441e1a1b5664e3e4fa8852236d8f2
Afuza <thegannoz@gmail.com>
2020-02-22 15:51:06 +0700

    Stored contents of About route in separate folder

---
+++ src/containers/About/AboutContent.js
@@ -0,0 +1,146 @@
+import React from "react";
+
+export const about = (
+  <p>
+    A prolific author and speaker, Alan Watts was one of the first to interpret
+    Eastern wisdom for a Western audience. Born outside London in 1915, he
+    discovered the nearby Buddhist Lodge at a young age. After moving to the
+    United States in 1938, Alan became an Episcopal priest for a time, and then
+    relocated to Millbrook, New York, where he wrote his pivotal book The Wisdom
+    of Insecurity: A Message for an Age of Anxiety. In 1951 he moved to San
+    Francisco where he began teaching Buddhist studies, and in 1956 began his
+    popular radio show, “Way Beyond the West.” By the early sixties, Alan’s
+    radio talks aired nationally and the counterculture movement adopted him as
+    a spiritual spokesperson. He wrote and traveled regularly until his passing
+    in 1973.
+  </p>
+);
+
+export const earlyLife = (
+  <p>
+    Watts was born to middle-class parents in the village of Chislehurst, Kent,
+    on 6 January 1915, living at 3 (now 5) Holbrook Lane, which was previously
+    lived in by author John Hemming-Clark in the early 1900s. Watts' father,
+    Laurence Wilson Watts, was a representative for the London office of the
+    Michelin tyre company. His mother, Emily Mary Watts, was a housewife whose
+    father had been a missionary. With modest financial means, they chose to
+    live in pastoral surroundings and Watts, an only child, grew up playing at
+    Brookside, learning the names of wildflowers and butterflies. Probably
+    because of the influence of his mother's religious family the Buchans, an
+    interest in "ultimate things" seeped in. It mixed with Watts's own interests
+    in storybook fables and romantic tales of the mysterious Far East. Watts
+    also later wrote of a mystical dream he experienced while ill with a fever
+    as a child.During this time he was influenced by Far Eastern landscape
+    paintings and embroideries that had been given to his mother by missionaries
+    returning from China. The few Chinese paintings Watts was able to see in
+    England riveted him, and he wrote "I was aesthetically fascinated with a
+    certain clarity, transparency, and spaciousness in Chinese and Japanese art.
+    It seemed to float..." These works of art emphasised the participatory
+    relationship of people in nature, a theme that stood fast throughout his
+    life and one that he often wrote about. (See, for instance, the last chapter
+    in The Way of Zen.)
+  </p>
+);
+
+export const middleYears = (
+  <React.Fragment>
+    <p>
+      Watts left the faculty for a career in the mid-1950s. In 1953, he began
+      what became a long-running weekly radio program at Pacifica Radio station
+      KPFA in Berkeley. Like other volunteer programmers at the
+      listener-sponsored station, Watts was not paid for his broadcasts. These
+      weekly broadcasts continued until 1962, by which time he had attracted a
+      "legion of regular listeners".
+    </p>
+
+    <p>
+      In 1957 Watts, then 42, published one of his best known books, The Way of
+      Zen, which focused on philosophical explication and history. Besides
+      drawing on the lifestyle and philosophical background of Zen in India and
+      China, Watts introduced ideas drawn from general semantics (directly from
+      the writings of Alfred Korzybski) and also from Norbert Wiener's early
+      work on cybernetics, which had recently been published. Watts offered
+      analogies from cybernetic principles possibly applicable to the Zen life.
+      The book sold well, eventually becoming a modern classic, and helped widen
+      his lecture circuit.
+    </p>
+
+    <p>
+      Though never affiliated for long with any one academic institution, he was
+      Professor of Comparative Philosophy at the California Institute of
+      Integral Studies (as mentioned above), had a fellowship at Harvard
+      University (1962–1964), and was a Scholar at San Jose State University
+      (1968). He also lectured to many college and university students as well
+      as the general public. His lectures and books gave him far-reaching
+      influence on the American intelligentsia of the 1950s–1970s, but he was
+      often seen as an outsider in academia. When questioned sharply by students
+      during his talk at University of California, Santa Cruz, in 1970, Watts
+      responded, as he had from the early sixties, that he was not an academic
+      philosopher but rather "a philosophical entertainer".
+    </p>
+  </React.Fragment>
+);
+
+export const laterYears = (
+  <React.Fragment>
+    <p>
+      In his writings of the 1950s, he conveyed his admiration for the
+      practicality in the historical achievements of Chán (Zen) in the Far East,
+      for it had fostered farmers, architects, builders, folk physicians,
+      artists, and administrators among the monks who had lived in the
+      monasteries of its lineages. In his mature work, he presents himself as
+      "Zennist" in spirit as he wrote in his last book, Tao: The Watercourse
+      Way. Child rearing, the arts, cuisine, education, law and freedom,
+      architecture, sexuality, and the uses and abuses of technology were all of
+      great interest to him.
+    </p>
+
+    <p>
+      Though known for his discourses on Zen, he was also influenced by ancient
+      Hindu scriptures, especially Vedanta. He spoke extensively about the
+      nature of the divine reality which Man misses: how the contradiction of
+      opposites is the method of life and the means of cosmic and human
+      evolution, how our fundamental Ignorance is rooted in the exclusive nature
+      of mind and ego, how to come in touch with the Field of Consciousness and
+      Light, and other cosmic principles. These are discussed in great detail in
+      dozens of hours of audio that are in part captured in the 'Out of Your
+      Mind' series.
+    </p>
+
+    <p>
+      Watts sought to resolve his feelings of alienation from the institutions
+      of marriage and the values of American society, as revealed in his classic
+      comments on love relationships in "Divine Madness" and on perception of
+      the organism-environment in "The Philosophy of Nature". In looking at
+      social issues he was quite concerned with the necessity for international
+      peace, for tolerance and understanding among disparate cultures.
+    </p>
+  </React.Fragment>
+);
+
+export const death = (
+  <React.Fragment>
+    <p>
+      In October 1973, Watts returned from a European lecture tour to his cabin
+      in Druid Heights, California. Friends of Watts had been concerned about
+      him for some time over his alcoholism. On 16 November 1973, at age 58, he
+      died in his sleep. He was reported to have been under treatment for a
+      heart condition. His body was cremated very shortly thereafter. His ashes
+      were split, with half buried near his library at Druid Heights and half at
+      the Green Gulch Monastery.
+    </p>
+
+    <p>
+      A personal account of Watts' last years and approach to death is given by
+      Al Chung-liang Huang in Tao: The Watercourse Way. His son Mark Watts has
+      prepared a biographical documentary that details questions surrounding his
+      father's death and performed ritual cremation on a nearby beach. His
+      father's ashes were returned to the cabin where he had died.
+    </p>
+
+    <p>
+      Since Watts' death, his lectures have found posthumous popularity on the
+      internet, specifically YouTube.
+    </p>
+  </React.Fragment>
+);
