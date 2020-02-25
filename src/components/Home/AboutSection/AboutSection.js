c07fe3992e176be9b49ffe6c912531d3f4fb1f75
Afuza <thegannoz@gmail.com>
2020-02-25 10:23:16 +0700

    Fixed bugs after revert

--- src/components/Home/AboutSection/AboutSection.js
+++ src/components/Home/AboutSection/AboutSection.js
@@ -9,7 +9,34 @@ import TopSectionChanger from "../../UI/TopSectionChanger/TopSectionChanger";
 import background from "../../../assets/images/background/alan-watts.jpg";
 
 const aboutSection = props => {
-  return <Section {...props}></Section>;
+  return (
+    <React.Fragment>
+      <TopSectionChanger color="white" topPosition={props.order} />
+      <section id="about" className={classes.AboutSection}>
+        <h3 className={classes.Title}>ABOUT</h3>
+        <div className={classes.Container}>
+          <p>
+            A prolific author and speaker, Alan Watts was one of the first to
+            interpret Eastern wisdom for a Western audience. Born outside London
+            in 1915, he discovered the nearby Buddhist Lodge at a young age.
+            After moving to the United States in 1938, Alan became an Episcopal
+            priest for a time, and then relocated to Millbrook, New York, where
+            he wrote his pivotal book The Wisdom of Insecurity: A Message for an
+            Age of Anxiety. In 1951 he moved to San Francisco where he began
+            teaching Buddhist studies, and in 1956 began his popular radio show,
+            “Way Beyond the West.” By the early sixties, Alan’s radio talks
+            aired nationally and the counterculture movement adopted him as a
+            spiritual spokesperson. He wrote and traveled regularly until his
+            passing in 1973.
+          </p>
+        </div>
+        <a href="/about" style={{ top: `${props.order}82vh` }}>
+          MORE ABOUT ALAN WATTS
+        </a>
+      </section>
+      <SectionChanger to="video" color="black" topPosition={props.order} />
+    </React.Fragment>
+  );
 };
 
 export default aboutSection;
