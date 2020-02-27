ec23e73cfd54af18e9fb3be9acdcc485bc7a52dc
Afuza <thegannoz@gmail.com>
2020-02-27 10:34:54 +0700

    Created Video route

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import Home from "./containers/Home/Home";
 import About from "./containers/About/About";
+import Video from "./containers/Video/Video";
 
 const app = () => {
   return (
     <Layout>
       <Switch>
         <Route path="/about" exact component={About} />
+        <Route path="/video" exact component={Video} />
         <Route path="/" exact component={Home} />
       </Switch>
     </Layout>
--- src/containers/Video/VideoContent.js
+++ src/containers/Video/VideoContent.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export const nuagesDreams = (
   <iframe
     width="560"
