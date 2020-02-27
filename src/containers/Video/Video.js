3790746149239569f71d99adc4537ad5e5d2aa3d
Afuza <thegannoz@gmail.com>
2020-02-27 10:29:36 +0700

    Created starter content for Video route

--- src/containers/Video/Video.js
+++ src/containers/Video/Video.js
@@ -2,8 +2,63 @@ import React, { Component } from "react";
 
 import { renderSections } from "../../shared/utility";
 
+import {
+  nuagesDreams,
+  realYou,
+  fallingInLove,
+  dreamOfLife
+} from "./VideoContent";
+
+import videoClasses from "./Styles/VideoSection.module.css";
+
+import background from "../../assets/images/background/home/star-wallpaper.jpg";
+
 class Video extends Component {
-  state = { sections: [] };
+  state = {
+    sections: [
+      {
+        id: "home",
+        title: "video",
+        background,
+        backgroundCover: true,
+        noTopSectionChanger: true,
+        color: "white",
+        classes: videoClasses
+      },
+      {
+        id: "nuagesDreams",
+        title: "dreams",
+        background,
+        mainContent: nuagesDreams,
+        color: "white",
+        classes: videoClasses
+      },
+      {
+        id: "realYou",
+        title: "the real you",
+        background,
+        mainContent: realYou,
+        color: "white",
+        classes: videoClasses
+      },
+      {
+        id: "fallingInLove",
+        title: "falling in love",
+        background,
+        mainContent: fallingInLove,
+        color: "white",
+        classes: videoClasses
+      },
+      {
+        id: "dreamOfLife",
+        title: "the dream of life",
+        background,
+        mainContent: dreamOfLife,
+        color: "white",
+        classes: videoClasses
+      }
+    ]
+  };
   render() {
     return (
       <React.Fragment>{renderSections(this.state.sections)}</React.Fragment>
---
+++ src/containers/Video/VideoContent.js
@@ -0,0 +1,43 @@
+export const nuagesDreams = (
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/9RMHHwJ9Eqk"
+    frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+);
+
+export const realYou = (
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/mMRrCYPxD0I"
+    frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+);
+
+export const fallingInLove = (
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/X1U4WzqH3YM"
+    frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+);
+
+export const dreamOfLife = (
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/wU0PYcCsL6o"
+    frameborder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+);
