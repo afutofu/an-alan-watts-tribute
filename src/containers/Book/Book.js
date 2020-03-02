29286025b51bf353672c65d6dad327069a8d6cf7
Afuza <thegannoz@gmail.com>
2020-03-02 08:32:24 +0700

    Added TheBook seciton to /book route

--- src/containers/Book/Book.js
+++ src/containers/Book/Book.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 
 import { renderSections } from "../../shared/utility";
 
-import { wisdomOfInsecurity } from "./BookContent";
+import { wisdomOfInsecurity, theBook } from "./BookContent";
 
 import homeBackground from "../../assets/images/background/book-grass.jpg";
 import wisdomOfInsecurityBackground from "../../assets/images/background/books.jpg";
+import theBookBackground from "../../assets/images/background/library-dim.jpg";
 
 import homeClasses from "./Styles/HomeSection.module.css";
 import wisdomOfInsecurityClasses from "./Styles/WisdomOfInsecurity.module.css";
+import theBookClasses from "./Styles/TheBook.module.css";
 
 class Book extends Component {
   state = {
@@ -30,20 +32,20 @@ class Book extends Component {
         mainContent: wisdomOfInsecurity,
         classes: wisdomOfInsecurityClasses,
         link:
-          "https://www.amazon.com/Wisdom-Insecurity-Message-Age-Anxiety/dp/0307741206/ref=sr_1_1?crid=3309VHJD1MNFF&keywords=the+wisdom+of+insecurity+alan+watts&qid=1582942402&sprefix=the+wisdom+of+insec%2Caps%2C382&sr=8-1",
-        linkText: "click here to order now",
+          "https://www.amazon.com/Wisdom-Insecurity-Message-Age-Anxiety/dp/0307741206/ref=sr_1_1?crid=3309VHJD1MNFF&qid=1582942402&sprefix=the+wisdom+of+insec%2Caps%2C382&sr=8-1",
+        linkText: "order the wisdom of insecurity",
         linkNewWindow: true
       },
       {
-        id: "wisdomOfInsecurity",
-        title: "the wisdom of insecurity",
-        background: wisdomOfInsecurityBackground,
-        color: "black",
-        mainContent: wisdomOfInsecurity,
-        classes: wisdomOfInsecurityClasses,
+        id: "thebook",
+        title: "the book: on the taboo against knowing who you are",
+        background: theBookBackground,
+        color: "white",
+        mainContent: theBook,
+        classes: theBookClasses,
         link:
-          "https://www.amazon.com/Wisdom-Insecurity-Message-Age-Anxiety/dp/0307741206/ref=sr_1_1?crid=3309VHJD1MNFF&keywords=the+wisdom+of+insecurity+alan+watts&qid=1582942402&sprefix=the+wisdom+of+insec%2Caps%2C382&sr=8-1",
-        linkText: "click here to order now",
+          "https://www.amazon.com/Book-Taboo-Against-Knowing-Who/dp/0679723005/ref=sr_1_4?crid=1I5W0J1BG6ZUQ&qid=1583109620&sprefix=alan+watts%2Caps%2C382&sr=8-4",
+        linkText: "order the book",
         linkNewWindow: true
       }
     ]
--- src/containers/Book/BookContent.js
+++ src/containers/Book/BookContent.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 import wisdomOfInsecurityClasses from "./Styles/WisdomOfInsecurity.module.css";
+import theBookClasses from "./Styles/TheBook.module.css";
+
+import wisdomOfInsecurityCover from "../../assets/images/books/TheWisdomOfInsecurity.jpg";
+import theBookCover from "../../assets/images/books/TheBook.jpg";
 
 export const wisdomOfInsecurity = (
   <React.Fragment>
@@ -19,6 +23,24 @@ export const wisdomOfInsecurity = (
         not—and cannot—know that we can learn anything truly worth knowing. In
         The Wisdom of Insecurity, he shows us how, in order to lead a fulfilling
         life, we must embrace the present—and live fully in the now.
+      </p>
+    </div>
+  </React.Fragment>
+);
+
+export const theBook = (
+  <React.Fragment>
+    <img className={theBookClasses.BookCover} src={theBookCover} alt="" />
+    <div className={theBookClasses.BookDesc}>
+      <p>
+        At the root of human conflict is our fundamental misunderstanding of who
+        we are. The illusion that we are isolated beings, unconnected to the
+        rest of the universe, has led us to view the “outside” world with
+        hostility, and has fueled our misuse of technology and our violent and
+        hostile subjugation of the natural world. To help us understand that the
+        self is in fact the root and ground of the universe, Watts has crafted a
+        revelatory primer on what it means to be human—and a mind-opening manual
+        of initiation into the central mystery of existence.
       </p>
     </div>
   </React.Fragment>
